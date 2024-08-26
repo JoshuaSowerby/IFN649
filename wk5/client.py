461beba124fa4a40c4f170604bc40b504ec911b1
JoshuaSowerby <josh.d.sowerby@gmail.com>
2024-08-26 13:37:50 +1000

    working week 5 files

--- wk5/client.py
+++ wk5/client.py
@@ -6,7 +6,11 @@ def on_connect(client, userdata, flags, rc): #func for making connection
           
     client.subscribe("ifn649")
 def on_message(client, userdata, msg): #func for sending message
-    print(msg.topic+""+str(msg.payload))
+    print(msg.topic+": "+(msg.payload).decode())#str.decode adds a b for bytes, this looks better but doesnt have as much info i guess
+    
+    cmd=str.enocde(" "+msg.decode()) #it keeps cutting of first letter so we add soemthign unimportant to cutt off
+    ser=serial.Serial("/dev/rfcomm0",9600)
+    ser.write(cmd)
 
 client = mqtt.Client()
 client.on_connect = on_connect
